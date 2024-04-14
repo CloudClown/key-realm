d20ad0d71ce65abe3c358bc78d4c1b9d4531c5fe
tahmid <radon1441@gmail.com>
2024-04-14 19:47:45 +0600

    created photo gallery

--- src/components/product-details/PhotoGallery.tsx
+++ src/components/product-details/PhotoGallery.tsx
@@ -1,9 +1,53 @@
+import Slider from "react-slick";
+import "./PhotoGallery.scss"; // Import the SCSS file
+import key1 from "../../assets/landing-page-img/keyboard-layout/60keyboard.webp";
+import key2 from "../../assets/landing-page-img/keyboard-layout/65keyboard.webp";
+import key3 from "../../assets/landing-page-img/keyboard-layout/75keyboard.webp";
+import key4 from "../../assets/landing-page-img/keyboard-layout/tklKeyboard.webp";
 
 const PhotoGallery = () => {
-  return (
-    <div className="w-[100%] bg-slate-500">
-      PhotoGallery</div>
-  )
-}
+  const keys = [key1, key2, key3, key4];
 
-export default PhotoGallery
+  const settings = {
+    customPaging: function (i: number) {
+      return (
+        <a className="inline-block">
+          <img
+            src={keys[i]}
+            alt={`key${i + 1}`}
+            className="w-32 h-auto rounded-lg border-2 border-gray-300"
+            style={{ width: "32px !important" }}
+          />
+        </a>
+      );
+    },
+    dots: true,
+    dotsClass: "slick-dots", // Remove the slick-thumb class
+    infinite: true,
+    speed: 500,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    arrows: false,
+  };
+
+  return (
+    <div className="container mx-auto">
+      <Slider {...settings}>
+        <div>
+          <img src={key1} alt="key1" />
+        </div>
+        <div>
+          <img src={key2} alt="key2" />
+        </div>
+        <div>
+          <img src={key3} alt="key3" />
+        </div>
+        <div>
+          <img src={key4} alt="key4" />
+        </div>
+      </Slider>
+    </div>
+  );
+};
+
+export default PhotoGallery;
